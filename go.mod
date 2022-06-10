37acfec324a5c9f87d7ad11b6093ed1aa8a7e068
anhongkui <anhongkui@jd.com>
2022-06-10 19:01:41 +0800

    update ; change mux => gin

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module demo
 
 go 1.16
 
-require github.com/gorilla/mux v1.8.0
+require github.com/gin-gonic/gin v1.8.1
--- main.go
+++ main.go
@@ -3,12 +3,12 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/gorilla/mux"
 	"io/ioutil"
 	"net/http"
 	"strings"
-	"text/template"
 	"time"
+
+	"github.com/gin-gonic/gin"
 )
 
 // GitHub App ClientID
@@ -51,9 +51,13 @@ func httpPost(url, body string) []byte {
 }
 
 type Token struct {
-	AccessToken string `json:"access_token"`
-	TokenType   string `json:"token_type"`
-	Scope       string `json:"scope"`
+	AccessToken string `json:"access_token,omitempty"`
+	TokenType   string `json:"token_type,omitempty"`
+	Scope       string `json:"scope,omitempty"`
+
+	Error            string `json:"error,omitempty"`
+	ErrorDescription string `json:"error_description,omitempty"`
+	ErrorUri         string `json:"error_uri,omitempty"`
 }
 
 type GithubUser struct {
@@ -92,37 +96,52 @@ type GithubUser struct {
 }
 
 // processApi
-func processApi(w http.ResponseWriter, r *http.Request) {
-	code := r.URL.Query().Get("code")
+func processApi(ctx *gin.Context) {
+	code := ctx.Query("code")
 
 	// 获取令牌
 	data := httpPost(fmt.Sprintf("https://github.com/login/oauth/access_token?client_id=%v&client_secret=%v&code=%v",
 		clientId, clientSecret, code), "")
 	token := &Token{}
-	json.Unmarshal(data, token)
+	_ = json.Unmarshal(data, token)
+
+	if token.Error != "" {
+		ctx.IndentedJSON(403, token)
+		return
+	}
 
 	user := &GithubUser{}
 	data2 := httpGet("https://api.github.com/user", token.AccessToken)
-	json.Unmarshal(data2, user)
-	w.Write(data2)
+	_ = json.Unmarshal(data2, user)
+	ctx.IndentedJSON(200, user)
 }
 
 // processHtml
-func processHtml(w http.ResponseWriter, r *http.Request) {
-	tpl := template.Must(template.ParseFiles("./html/index.html"))
-	tpl.Execute(w, nil)
+func processHtml(ctx *gin.Context) {
+	_, _ = ctx.Writer.Write([]byte(`
+<head>
+    <title>HelloWorld</title>
+</head>
+
+<body>
+    <a href="https://github.com/login/oauth/authorize?client_id=dc7c2a2035486454e400">Github 登录</a>
+</body>
+`))
 }
 
 // 启用Web
 func startWeb() {
-	router := mux.NewRouter()
-	router.HandleFunc("/api/go", processApi)
-	router.PathPrefix("/css").Handler(http.StripPrefix("/css/", http.FileServer(http.Dir("./html/css"))))
-	router.HandleFunc("/", processHtml)
-
-	if err := http.ListenAndServe(":8080", router); err != nil {
-		panic(err)
-	}
+	router := gin.Default()
+	router.GET("/", processHtml)
+	router.GET("/api/go", processApi)
+	//router.HandleFunc("/api/go", processApi)
+	//router.PathPrefix("/css").Handler(http.StripPrefix("/css/", http.FileServer(http.Dir("./html/css"))))
+	//router.HandleFunc("/", processHtml)
+	//
+	//if err := http.ListenAndServe(":8080", router); err != nil {
+	//	panic(err)
+	//}
+	_ = router.Run(":8080")
 }
 
 //main
