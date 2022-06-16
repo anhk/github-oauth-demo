b92ad5dbfc7af103ec74978da437682c83bf69d7
anhongkui <anhongkui@jd.com>
2022-06-16 14:57:04 +0800

    support github & aliyun oauth2

--- main.go
+++ main.go
@@ -6,15 +6,25 @@ import (
 	"io/ioutil"
 	"net/http"
 	"strings"
-	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
 // GitHub App ClientID
 const (
-	clientId     = "dc7c2a2035486454e400"
-	clientSecret = "9bbb968691e817399a5000bf7d83d3f532c9712e"
+	// GitHub
+	AccessTokenUrl = "https://github.com/login/oauth/access_token"
+	UserInfoUrl    = "https://api.github.com/user"
+	AuthorizeUrl   = "https://github.com/login/oauth/authorize"
+	ClientId       = "dc7c2a2035486454e400"
+	ClientSecret   = "9bbb968691e817399a5000bf7d83d3f532c9712e"
+
+	// AliYun
+	//AccessTokenUrl = "https://eiam-api-cn-hangzhou.aliyuncs.com/v2/idaas_abt3pfwojojcq323si6g5tx7e4/app_mkvmrajqr7e6sehnu7vlynet6a/oauth2/token"
+	//UserInfoUrl    = "https://eiam-api-cn-hangzhou.aliyuncs.com/v2/idaas_abt3pfwojojcq323si6g5tx7e4/app_mkvmrajqr7e6sehnu7vlynet6a/oauth2/userinfo"
+	//AuthorizeUrl   = "https://0hyuelcn.aliyunidaas.com/login/app/app_mkvmrajqr7e6sehnu7vlynet6a/oauth2/authorize"
+	//ClientId       = "app_mkvmrajqr7e6sehnu7vlynet6a"
+	//ClientSecret   = "CS2mfMVnRXJ11SkAdDYN3aedWdoTb9zjCiMVXdkn3LcvEW"
 )
 
 //
@@ -60,39 +70,11 @@ type Token struct {
 	ErrorUri         string `json:"error_uri,omitempty"`
 }
 
-type GithubUser struct {
-	Login             string    `json:"login"`
-	ID                int       `json:"id"`
-	NodeID            string    `json:"node_id"`
-	AvatarURL         string    `json:"avatar_url"`
-	GravatarID        string    `json:"gravatar_id"`
-	URL               string    `json:"url"`
-	HTMLURL           string    `json:"html_url"`
-	FollowersURL      string    `json:"followers_url"`
-	FollowingURL      string    `json:"following_url"`
-	GistsURL          string    `json:"gists_url"`
-	StarredURL        string    `json:"starred_url"`
-	SubscriptionsURL  string    `json:"subscriptions_url"`
-	OrganizationsURL  string    `json:"organizations_url"`
-	ReposURL          string    `json:"repos_url"`
-	EventsURL         string    `json:"events_url"`
-	ReceivedEventsURL string    `json:"received_events_url"`
-	Type              string    `json:"type"`
-	SiteAdmin         bool      `json:"site_admin"`
-	Name              string    `json:"name"`
-	Company           string    `json:"company"`
-	Blog              string    `json:"blog"`
-	Location          string    `json:"location"`
-	Email             string    `json:"email"`
-	Hireable          bool      `json:"hireable"`
-	Bio               string    `json:"bio"`
-	TwitterUsername   string    `json:"twitter_username"`
-	PublicRepos       int       `json:"public_repos"`
-	PublicGists       int       `json:"public_gists"`
-	Followers         int       `json:"followers"`
-	Following         int       `json:"following"`
-	CreatedAt         time.Time `json:"created_at"`
-	UpdatedAt         time.Time `json:"updated_at"`
+type UserInfo struct {
+	Login             string `json:"login"` // ==> Github
+	Name              string `json:"name"`
+	Email             string `json:"email"`
+	PreferredUsername string `json:"preferred_username"`
 }
 
 // processApi
@@ -100,8 +82,8 @@ func processApi(ctx *gin.Context) {
 	code := ctx.Query("code")
 
 	// 获取令牌
-	data := httpPost(fmt.Sprintf("https://github.com/login/oauth/access_token?client_id=%v&client_secret=%v&code=%v",
-		clientId, clientSecret, code), "")
+	data := httpPost(fmt.Sprintf("%s?grant_type=authorization_code&client_id=%v&client_secret=%v&code=%v&redirect_uri=",
+		AccessTokenUrl, ClientId, ClientSecret, code), "")
 	token := &Token{}
 	_ = json.Unmarshal(data, token)
 
@@ -110,34 +92,31 @@ func processApi(ctx *gin.Context) {
 		return
 	}
 
-	user := &GithubUser{}
-	data2 := httpGet("https://api.github.com/user", token.AccessToken)
+	user := &UserInfo{}
+	data2 := httpGet(UserInfoUrl, token.AccessToken)
+	fmt.Println(string(data2))
 	_ = json.Unmarshal(data2, user)
 	ctx.IndentedJSON(200, user)
 }
 
-// processHtml
-func processHtml(ctx *gin.Context) {
-	_, _ = ctx.Writer.Write([]byte(`
+var HtmlFmt = `
 <head>
     <title>HelloWorld</title>
 </head>
 
 <body>
-    <a href="https://github.com/login/oauth/authorize?client_id=dc7c2a2035486454e400">Github 登录</a>
+    <a href="%s?client_id=%s">OAuth2 登录</a>
 </body>
-`))
+`
+
+// processHtml
+func processHtml(ctx *gin.Context) {
+	_, _ = ctx.Writer.Write([]byte(fmt.Sprintf(HtmlFmt, AuthorizeUrl, ClientId)))
 }
 
-// 启用Web
-func startWeb() {
+func main() {
 	router := gin.Default()
 	router.GET("/", processHtml)
 	router.GET("/api/go", processApi) // 在Github上注册应用的时候，回调接口设置的是 http://127.1:8080/api/go
 	_ = router.Run(":8080")
-}
-
-//main
-func main() {
-	startWeb()
 }
